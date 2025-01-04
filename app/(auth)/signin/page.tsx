15cc610f22b835aee82089e04b24970644ddb768
Dennis <dennisndegwa001@gmail.com>
2025-01-04 09:51:58 +0300

    Initial Commit

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+// export const metadata = {
+//   title: 'Sign In - Mosaic',
+//   description: 'Page description',
+// }
+
+import Link from 'next/link'
+import AuthHeader from '../auth-header'
+import AuthImage from '../auth-image'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function SignIn() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://20.19.33.27:8000/api/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log(data, "Data-----31----")
+      if (response.ok) {
+        localStorage.setItem('access_token', data.access);
+        localStorage.setItem('refresh_token', data.refresh);
+        router.push('/dashboard');
+      } else {
+        setError(data.detail || 'Login failed');
+      }
+    } catch (err) {
+      setError('An error occurred during login');
+    }
+  };
+  return (
+    <main className="bg-white dark:bg-gray-900">
+
+      <div className="relative md:flex">
+
+        {/* Content */}
+        <div className="md:w-1/2">
+          <div className="min-h-[100dvh] h-full flex flex-col after:flex-1">
+
+            <AuthHeader />
+
+            <div className="max-w-sm mx-auto w-full px-4 py-8">
+              <h1 className="text-3xl text-gray-800 dark:text-gray-100 font-bold mb-6">Welcome back!</h1>
+              {/* Form */}
+              <form onSubmit={handleSubmit}>
+                <div className="space-y-4">
+                  <div>
+                    <label className="block text-sm font-medium mb-1" htmlFor="email">Email Address</label>
+                    <input 
+                    id="email" 
+                    className="form-input w-full" 
+                    type="email"        
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-sm font-medium mb-1" htmlFor="password">Password</label>
+                    <input 
+                    id="password" 
+                    className="form-input w-full" 
+                    type="password" autoComplete="on"        
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center justify-between mt-6">
+                  <div className="mr-1">
+                    <Link className="text-sm underline hover:no-underline" href="/reset-password">Forgot Password?</Link>
+                  </div>
+                  <button type='submit' className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white ml-3">Sign In</button>
+                </div>
+              </form>
+              {/* Footer */}
+              <div className="pt-5 mt-6 border-t border-gray-100 dark:border-gray-700/60">
+                <div className="text-sm">
+                  Don't you have an account? <Link className="font-medium text-violet-500 hover:text-violet-600 dark:hover:text-violet-400" href="/signup">Sign Up</Link>
+                </div>
+                {/* Warning */}
+                <div className="mt-5">
+                  <div className="bg-yellow-500/20 text-yellow-700 px-3 py-2 rounded-lg">
+                    <svg className="inline w-3 h-3 shrink-0 fill-current mr-2" viewBox="0 0 12 12">
+                      <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
+                    </svg>
+                    <span className="text-sm">
+                      To support you during the pandemic super pro features are free until March 31st.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <AuthImage />
+
+      </div>
+
+    </main>
+  )
+}
